d48e6ee9c6866a8b686604628108f5fecb19aaa9
Akshay GH <dualdeveloper002@gmail.com>
2025-03-26 20:35:26 +0530

    routes added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Signin from "./Pages/Signin";
+import Signup from "./Pages/Signup"; 
 
 function App() {
-  
-
   return (
-  <div></div>
-      
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
+
