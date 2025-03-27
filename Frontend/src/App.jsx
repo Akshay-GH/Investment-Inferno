3a6cadd90d03e626f19f3e0b01c21d46d9f83e54
Lovish1201 <lovishshrail10@gmail.com>
2025-03-27 14:23:38 +0530

    added landing and stocklist page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,16 +4,22 @@ import Signup from "./Pages/Signup";
 import Landing from "./Pages/Landing";
 import Stocklist from "./Pages/Stocklist";
 import StockChart from "./Pages/StockChart";
+import Navbar from "./components/Navbar";
+import Portfolio from "./Pages/Portfolio";
+import StarBackground from "./components/StarBackground";
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
+      <StarBackground /> 
       <Routes>
         <Route path="/signin" element={<Signin />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/" element={<Landing />} />
         <Route path="/stocklist" element={<Stocklist />} />
         <Route path="/stockchart" element={<StockChart />} />
+        <Route path="/portfolio" element={<Portfolio />} />
 
 
       </Routes>
